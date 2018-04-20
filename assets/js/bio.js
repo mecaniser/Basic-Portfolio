4145f99c0005ad083ccec67144fce7c629834127
Sergio <sergioburca@gmail.com>
2018-04-20 04:38:14 -0400

    Re-style with materialize Initialization

---
+++ assets/js/bio.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    // $('.parallax').parallax();
+    // M.updateTextFields();
+    M.AutoInit();
+    // $('.carousel').carousel();
+    // $(".dropdown-trigger").dropdown();
+    // $('.parallax').parallax();
+    // $('.materialboxed').materialbox();
+    // $('.tabs').tabs();
+});
