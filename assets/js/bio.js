9848c5b101bb3434bd86b763a74c00ce147fd43b
Sergio <sergioburca@gmail.com>
2018-04-23 02:56:45 -0400

    Minor updates

--- assets/js/bio.js
+++ assets/js/bio.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $('.tooltipped').tooltip({
         position: "top"
     });
+    $('.dropdown-trigger').dropdown();
     $('.fixed-action-btn').floatingActionButton({
         direction:"left",
     });
